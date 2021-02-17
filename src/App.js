7b61a6cd9b525d16c796760b94966d04a974b655
Drantho <drantho@gmail.com>
2021-02-16 19:59:42 -0800

    utils created, tested

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-function App() {
-  return (
-    <div>
-      
-      
-    </div>
-  );
-}
+import React, { Component } from 'react'
+import API from "./utils/API"
 
-export default App;
+export default class App extends Component {
+  state = {
+    employees: []
+  };
+
+  getEmployees = () => {
+    API.list().then(results => {
+      console.log(results.data.results);
+      this.setState({
+        employees: results.data.results
+      })
+    })
+  }
+
+  componentDidMount = () => {
+    this.getEmployees();
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+        {this.state.employees.map(employee => <li key={employee.cell}>{employee.name.title} {employee.name.first} {employee.name.last}</li>)}
+        </ul>
+      </div>
+    )
+  }
+}
